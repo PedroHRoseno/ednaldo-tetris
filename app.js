f65345f75470d5b57ad62f00c1ade6db16d9d0db
pedrohroseno <phrbs@cin.ufpe.br>
2021-03-14 02:28:46 -0300

    Adding time and intervals

--- app.js
+++ app.js
@@ -45,23 +45,52 @@ document.addEventListener('DOMContentLoaded', () => {
   const tetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
 
   let currentPosition = 4
+  let currentRotation = 0
   // Selecionar um tetraminó aleatório
   let randomNumber = Math.floor(Math.random()*tetrominoes.length)
   let current = tetrominoes[randomNumber][0]
 
   // desenhando o tetraminó
 
-  function draw(){
-    current.forEach( index => {
+  function draw () {
+    current.forEach(index => {
       squares[currentPosition + index].classList.add('tetromino')
     })
   }
-  function undraw(){
-    current.forEach( index => {
+
+  // apagando o tetraminó
+
+  function undraw () {
+    current.forEach(index => {
       squares[currentPosition + index].classList.remove('tetromino')
     })
   }
-  draw()
 
+  // movendo o tetraminó para baixo a cada 1 segundo
+  timerId = setInterval(moveDown, 1000)
+  function moveDown () {
+    undraw()
+    currentPosition += width
+    draw()
+    freeze()
+  }
+
+  // função auxiliar para iniciar um novo tetraminó
+
+  function startAgain () {
+    random = Math.floor(Math.random() * tetrominoes.length)
+    current = tetrominoes[random][currentRotation]
+    currentPosition = 4
+    draw()
+  }
+
+  // função de congelar o tetraminó
+
+  function freeze () {
+    if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+      current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+      startAgain()
+    }
+  }
 
 })
