375beb224220d095c808aca88de565f68c7642d7
pedrohroseno <phrbs@cin.ufpe.br>
2021-03-14 18:05:37 -0300

    Added function moveLeft()

--- app.js
+++ app.js
@@ -93,4 +93,17 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
+  // mover o tetraminó para a esquerda, até o limite do grid
+
+  function moveLeft () {
+    undraw()
+    const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+    if (!isAtLeftEdge) currentPosition -= 1
+
+    if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPosition += 1
+    }
+    draw()
+
+  }
 })
