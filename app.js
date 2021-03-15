4aaecf2850cc16a0e72c6a157b0d7063cbf01430
pedrohroseno <phrbs@cin.ufpe.br>
2021-03-15 01:28:47 -0300

    Finished the entire game logic

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const width = 10
   let nextRandom = 0
   let timerId
+  let score = 0
 
   //The Tetrominoes
   const lTetromino = [
@@ -100,6 +101,8 @@ document.addEventListener('DOMContentLoaded', () => {
       currentPosition = 4
       draw()
       displayShape()
+      addScore()
+      gameOver()
     }
   }
 
@@ -173,8 +176,7 @@ document.addEventListener('DOMContentLoaded', () => {
       displaySquares[displayIndex + index].classList.add('tetromino')
     })
   }
-
-  //add functionality to the button
+  // botão de iniciar e parar
   startbutton.addEventListener('click', () => {
     if (timerId) {
       clearInterval(timerId)
@@ -187,5 +189,26 @@ document.addEventListener('DOMContentLoaded', () => {
       displayShape()
     }
   })
+  //adicionar pontuação
+  function addScore(){
+    for (let i=0;i<199; i += width) {
+      const row = [i, i + 1, i + 2, i + 3, i + 4, i + 5, i + 6, i + 7, i + 8, i + 9]
+      if(row.every(index => squares[index].classList.contains('taken'))){
+        score += 10
+        scoreDisplay.innerHTML = score
+        row.forEach(index => {squares[index].classList.remove('taken'), squares[index].classList.remove('tetromino')})
+        const squaresRemoved = squares.splice(i, width)
+        squares = squaresRemoved.concat(squares)
+        squares.forEach(cell => grid.appendChild(cell))
+      }
+    }
+  }
+
+  function gameOver() {
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))){
+      alert("Game Over")
+      clearInterval(timerId)
+    }
+  }
 
 })
